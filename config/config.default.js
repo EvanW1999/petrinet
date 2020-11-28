30205fbd9e1b962dc30635237cfd82852693d799
Evan Wang <evan.l.wang@vanderbilt.edu>
2020-11-28 15:03:48 -0500

    Create base Plugin and Icore

--- config/config.default.js
+++ config/config.default.js
@@ -1,11 +1,12 @@
-'use strict';
+"use strict";
 
-var config = require('./config.webgme'),
-    validateConfig = require('webgme/config/validator');
+var config = require("./config.webgme"),
+  validateConfig = require("webgme/config/validator");
 
 // Add/overwrite any additional settings here
 // config.server.port = 8080;
-// config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+config.mongo.uri = 'mongodb://127.0.0.1:32768/webgme_my_app';
+config.plugin.allowServerExecution = true;
 
 validateConfig(config);
 module.exports = config;
