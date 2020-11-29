bd4a95ece5b74f4a9f874ba917a14a11e166ea31
Evan Wang <evan.l.wang@vanderbilt.edu>
2020-11-28 23:27:42 -0500

    Implement Part of Viz

--- src/visualizers/panels/PetrinetVisualization/PetrinetVisualizationPanel.js
+++ src/visualizers/panels/PetrinetVisualization/PetrinetVisualizationPanel.js
@@ -4,96 +4,102 @@
  */
 
 define([
-    'js/PanelBase/PanelBaseWithHeader',
-    'js/PanelManager/IActivePanel',
-    'widgets/PetrinetVisualization/PetrinetVisualizationWidget',
-    './PetrinetVisualizationControl'
+  "js/PanelBase/PanelBaseWithHeader",
+  "js/PanelManager/IActivePanel",
+  "widgets/PetrinetVisualization/PetrinetVisualizationWidget",
+  "./PetrinetVisualizationControl",
 ], function (
-    PanelBaseWithHeader,
-    IActivePanel,
-    PetrinetVisualizationWidget,
-    PetrinetVisualizationControl
+  PanelBaseWithHeader,
+  IActivePanel,
+  PetrinetVisualizationWidget,
+  PetrinetVisualizationControl
 ) {
-    'use strict';
+  "use strict";
 
-    function PetrinetVisualizationPanel(layoutManager, params) {
-        var options = {};
-        //set properties from options
-        options[PanelBaseWithHeader.OPTIONS.LOGGER_INSTANCE_NAME] = 'PetrinetVisualizationPanel';
-        options[PanelBaseWithHeader.OPTIONS.FLOATING_TITLE] = true;
+  function PetrinetVisualizationPanel(layoutManager, params) {
+    var options = {};
+    //set properties from options
+    options[PanelBaseWithHeader.OPTIONS.LOGGER_INSTANCE_NAME] =
+      "PetrinetVisualizationPanel";
+    options[PanelBaseWithHeader.OPTIONS.FLOATING_TITLE] = true;
 
-        //call parent's constructor
-        PanelBaseWithHeader.apply(this, [options, layoutManager]);
+    //call parent's constructor
+    PanelBaseWithHeader.apply(this, [options, layoutManager]);
 
-        this._client = params.client;
+    this._client = params.client;
 
-        //initialize UI
-        this._initialize();
+    //initialize UI
+    this._initialize();
 
-        this.logger.debug('ctor finished');
-    }
+    this.logger.debug("ctor finished");
+  }
 
-    //inherit from PanelBaseWithHeader
-    _.extend(PetrinetVisualizationPanel.prototype, PanelBaseWithHeader.prototype);
-    _.extend(PetrinetVisualizationPanel.prototype, IActivePanel.prototype);
+  //inherit from PanelBaseWithHeader
+  _.extend(PetrinetVisualizationPanel.prototype, PanelBaseWithHeader.prototype);
+  _.extend(PetrinetVisualizationPanel.prototype, IActivePanel.prototype);
 
-    PetrinetVisualizationPanel.prototype._initialize = function () {
-        var self = this;
+  PetrinetVisualizationPanel.prototype._initialize = function () {
+    var self = this;
 
-        //set Widget title
-        this.setTitle('');
+    //set Widget title
+    this.setTitle("");
 
-        this.widget = new PetrinetVisualizationWidget(this.logger, this.$el);
+    this.widget = new PetrinetVisualizationWidget(
+      this.logger,
+      this.$el,
+      this._client
+    );
 
-        this.widget.setTitle = function (title) {
-            self.setTitle(title);
-        };
-
-        this.control = new PetrinetVisualizationControl({
-            logger: this.logger,
-            client: this._client,
-            widget: this.widget
-        });
-
-        this.onActivate();
+    this.widget.setTitle = function (title) {
+      self.setTitle(title);
     };
 
-    /* OVERRIDE FROM WIDGET-WITH-HEADER */
-    /* METHOD CALLED WHEN THE WIDGET'S READ-ONLY PROPERTY CHANGES */
-    PetrinetVisualizationPanel.prototype.onReadOnlyChanged = function (isReadOnly) {
-        //apply parent's onReadOnlyChanged
-        PanelBaseWithHeader.prototype.onReadOnlyChanged.call(this, isReadOnly);
+    this.control = new PetrinetVisualizationControl({
+      logger: this.logger,
+      client: this._client,
+      widget: this.widget,
+    });
 
-    };
+    this.onActivate();
+  };
 
-    PetrinetVisualizationPanel.prototype.onResize = function (width, height) {
-        this.logger.debug('onResize --> width: ' + width + ', height: ' + height);
-        this.widget.onWidgetContainerResize(width, height);
-    };
+  /* OVERRIDE FROM WIDGET-WITH-HEADER */
+  /* METHOD CALLED WHEN THE WIDGET'S READ-ONLY PROPERTY CHANGES */
+  PetrinetVisualizationPanel.prototype.onReadOnlyChanged = function (
+    isReadOnly
+  ) {
+    //apply parent's onReadOnlyChanged
+    PanelBaseWithHeader.prototype.onReadOnlyChanged.call(this, isReadOnly);
+  };
 
-    /* * * * * * * * Visualizer life cycle callbacks * * * * * * * */
-    PetrinetVisualizationPanel.prototype.destroy = function () {
-        this.control.destroy();
-        this.widget.destroy();
+  PetrinetVisualizationPanel.prototype.onResize = function (width, height) {
+    this.logger.debug("onResize --> width: " + width + ", height: " + height);
+    this.widget.onWidgetContainerResize(width, height);
+  };
 
-        PanelBaseWithHeader.prototype.destroy.call(this);
-        WebGMEGlobal.KeyboardManager.setListener(undefined);
-        WebGMEGlobal.Toolbar.refresh();
-    };
+  /* * * * * * * * Visualizer life cycle callbacks * * * * * * * */
+  PetrinetVisualizationPanel.prototype.destroy = function () {
+    this.control.destroy();
+    this.widget.destroy();
 
-    PetrinetVisualizationPanel.prototype.onActivate = function () {
-        this.widget.onActivate();
-        this.control.onActivate();
-        WebGMEGlobal.KeyboardManager.setListener(this.widget);
-        WebGMEGlobal.Toolbar.refresh();
-    };
+    PanelBaseWithHeader.prototype.destroy.call(this);
+    WebGMEGlobal.KeyboardManager.setListener(undefined);
+    WebGMEGlobal.Toolbar.refresh();
+  };
 
-    PetrinetVisualizationPanel.prototype.onDeactivate = function () {
-        this.widget.onDeactivate();
-        this.control.onDeactivate();
-        WebGMEGlobal.KeyboardManager.setListener(undefined);
-        WebGMEGlobal.Toolbar.refresh();
-    };
+  PetrinetVisualizationPanel.prototype.onActivate = function () {
+    this.widget.onActivate();
+    this.control.onActivate();
+    WebGMEGlobal.KeyboardManager.setListener(this.widget);
+    WebGMEGlobal.Toolbar.refresh();
+  };
 
-    return PetrinetVisualizationPanel;
+  PetrinetVisualizationPanel.prototype.onDeactivate = function () {
+    this.widget.onDeactivate();
+    this.control.onDeactivate();
+    WebGMEGlobal.KeyboardManager.setListener(undefined);
+    WebGMEGlobal.Toolbar.refresh();
+  };
+
+  return PetrinetVisualizationPanel;
 });
