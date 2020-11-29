ffdb017519ea2873a97230d34ad19997c29b352a
Evan Wang <evan.l.wang@vanderbilt.edu>
2020-11-28 21:54:11 -0500

    Update viz information

--- src/visualizers/panels/PetrinetVisualization/PetrinetVisualizationControl.js
+++ src/visualizers/panels/PetrinetVisualization/PetrinetVisualizationControl.js
@@ -4,244 +4,251 @@
  */
 
 define([
-    'js/Constants',
-    'js/Utils/GMEConcepts',
-    'js/NodePropertyNames'
-], function (
-    CONSTANTS,
-    GMEConcepts,
-    nodePropertyNames
-) {
+  "js/Constants",
+  "js/Utils/GMEConcepts",
+  "js/NodePropertyNames",
+], function (CONSTANTS, GMEConcepts, nodePropertyNames) {
+  "use strict";
 
-    'use strict';
+  function PetrinetVisualizationControl(options) {
+    this._logger = options.logger.fork("Control");
 
-    function PetrinetVisualizationControl(options) {
+    this._client = options.client;
 
-        this._logger = options.logger.fork('Control');
+    // Initialize core collections and variables
+    this._widget = options.widget;
 
-        this._client = options.client;
+    this._currentNodeId = null;
+    this._currentNodeParentId = undefined;
 
-        // Initialize core collections and variables
-        this._widget = options.widget;
+    this._initWidgetEventHandlers();
 
-        this._currentNodeId = null;
-        this._currentNodeParentId = undefined;
+    this._logger.debug("ctor finished");
+  }
 
-        this._initWidgetEventHandlers();
+  PetrinetVisualizationControl.prototype._initWidgetEventHandlers = function () {
+    this._widget.onNodeClick = function (id) {
+      // Change the current active object
+      WebGMEGlobal.State.registerActiveObject(id);
+    };
+  };
 
-        this._logger.debug('ctor finished');
+  /* * * * * * * * Visualizer content update callbacks * * * * * * * */
+  // One major concept here is with managing the territory. The territory
+  // defines the parts of the project that the visualizer is interested in
+  // (this allows the browser to then only load those relevant parts).
+  PetrinetVisualizationControl.prototype.selectedObjectChanged = function (
+    nodeId
+  ) {
+    var desc = this._getObjectDescriptor(nodeId),
+      self = this;
+
+    self._logger.debug("activeObject nodeId '" + nodeId + "'");
+
+    // Remove current territory patterns
+    if (self._currentNodeId) {
+      self._client.removeUI(self._territoryId);
     }
 
-    PetrinetVisualizationControl.prototype._initWidgetEventHandlers = function () {
-        this._widget.onNodeClick = function (id) {
-            // Change the current active object
-            WebGMEGlobal.State.registerActiveObject(id);
-        };
-    };
+    self._currentNodeId = nodeId;
+    self._currentNodeParentId = undefined;
 
-    /* * * * * * * * Visualizer content update callbacks * * * * * * * */
-    // One major concept here is with managing the territory. The territory
-    // defines the parts of the project that the visualizer is interested in
-    // (this allows the browser to then only load those relevant parts).
-    PetrinetVisualizationControl.prototype.selectedObjectChanged = function (nodeId) {
-        var desc = this._getObjectDescriptor(nodeId),
-            self = this;
+    if (typeof self._currentNodeId === "string") {
+      // Put new node's info into territory rules
+      self._selfPatterns = {};
+      self._selfPatterns[nodeId] = { children: 0 }; // Territory "rule"
 
-        self._logger.debug('activeObject nodeId \'' + nodeId + '\'');
+      self._widget.setTitle(desc.name.toUpperCase());
 
-        // Remove current territory patterns
-        if (self._currentNodeId) {
-            self._client.removeUI(self._territoryId);
-        }
+      if (typeof desc.parentId === "string") {
+        self.$btnModelHierarchyUp.show();
+      } else {
+        self.$btnModelHierarchyUp.hide();
+      }
 
-        self._currentNodeId = nodeId;
-        self._currentNodeParentId = undefined;
+      self._currentNodeParentId = desc.parentId;
 
-        if (typeof self._currentNodeId === 'string') {
-            // Put new node's info into territory rules
-            self._selfPatterns = {};
-            self._selfPatterns[nodeId] = {children: 0};  // Territory "rule"
+      self._territoryId = self._client.addUI(self, function (events) {
+        self._eventCallback(events);
+      });
 
-            self._widget.setTitle(desc.name.toUpperCase());
+      // Update the territory
+      self._client.updateTerritory(self._territoryId, self._selfPatterns);
 
-            if (typeof desc.parentId === 'string') {
-                self.$btnModelHierarchyUp.show();
-            } else {
-                self.$btnModelHierarchyUp.hide();
-            }
+      self._selfPatterns[nodeId] = { children: 1 };
+      self._client.updateTerritory(self._territoryId, self._selfPatterns);
+    }
+  };
 
-            self._currentNodeParentId = desc.parentId;
+  // This next function retrieves the relevant node information for the widget
+  PetrinetVisualizationControl.prototype._getObjectDescriptor = function (
+    nodeId
+  ) {
+    var node = this._client.getNode(nodeId),
+      objDescriptor;
+    if (node) {
+      objDescriptor = {
+        id: node.getId(),
+        name: node.getAttribute(nodePropertyNames.Attributes.name),
+        meta: node.getAttribute(nodePropertyNames.Attributes.meta),
+        childrenIds: node.getChildrenIds(),
+        parentId: node.getParentId(),
+        isConnection: GMEConcepts.isConnection(nodeId),
+      };
+    }
 
-            self._territoryId = self._client.addUI(self, function (events) {
-                self._eventCallback(events);
-            });
+    return objDescriptor;
+  };
 
-            // Update the territory
-            self._client.updateTerritory(self._territoryId, self._selfPatterns);
+  /* * * * * * * * Node Event Handling * * * * * * * */
+  PetrinetVisualizationControl.prototype._eventCallback = function (events) {
+    var i = events ? events.length : 0,
+      event;
 
-            self._selfPatterns[nodeId] = {children: 1};
-            self._client.updateTerritory(self._territoryId, self._selfPatterns);
-        }
-    };
+    this._logger.debug("_eventCallback '" + i + "' items");
 
-    // This next function retrieves the relevant node information for the widget
-    PetrinetVisualizationControl.prototype._getObjectDescriptor = function (nodeId) {
-        var node = this._client.getNode(nodeId),
-            objDescriptor;
-        if (node) {
-            objDescriptor = {
-                id: node.getId(),
-                name: node.getAttribute(nodePropertyNames.Attributes.name),
-                childrenIds: node.getChildrenIds(),
-                parentId: node.getParentId(),
-                isConnection: GMEConcepts.isConnection(nodeId)
-            };
-        }
+    while (i--) {
+      event = events[i];
+      switch (event.etype) {
+        case CONSTANTS.TERRITORY_EVENT_LOAD:
+          this._onLoad(event.eid);
+          break;
+        case CONSTANTS.TERRITORY_EVENT_UPDATE:
+          this._onUpdate(event.eid);
+          break;
+        case CONSTANTS.TERRITORY_EVENT_UNLOAD:
+          this._onUnload(event.eid);
+          break;
+        default:
+          break;
+      }
+    }
 
-        return objDescriptor;
-    };
+    this._logger.debug("_eventCallback '" + events.length + "' items - DONE");
+  };
 
-    /* * * * * * * * Node Event Handling * * * * * * * */
-    PetrinetVisualizationControl.prototype._eventCallback = function (events) {
-        var i = events ? events.length : 0,
-            event;
+  PetrinetVisualizationControl.prototype._onLoad = function (gmeId) {
+    var description = this._getObjectDescriptor(gmeId);
+    this._widget.addNode(description);
+  };
 
-        this._logger.debug('_eventCallback \'' + i + '\' items');
+  PetrinetVisualizationControl.prototype._onUpdate = function (gmeId) {
+    var description = this._getObjectDescriptor(gmeId);
+    this._widget.updateNode(description);
+  };
 
-        while (i--) {
-            event = events[i];
-            switch (event.etype) {
+  PetrinetVisualizationControl.prototype._onUnload = function (gmeId) {
+    this._widget.removeNode(gmeId);
+  };
 
-            case CONSTANTS.TERRITORY_EVENT_LOAD:
-                this._onLoad(event.eid);
-                break;
-            case CONSTANTS.TERRITORY_EVENT_UPDATE:
-                this._onUpdate(event.eid);
-                break;
-            case CONSTANTS.TERRITORY_EVENT_UNLOAD:
-                this._onUnload(event.eid);
-                break;
-            default:
-                break;
-            }
-        }
+  PetrinetVisualizationControl.prototype._stateActiveObjectChanged = function (
+    model,
+    activeObjectId
+  ) {
+    if (this._currentNodeId === activeObjectId) {
+      // The same node selected as before - do not trigger
+    } else {
+      this.selectedObjectChanged(activeObjectId);
+    }
+  };
 
-        this._logger.debug('_eventCallback \'' + events.length + '\' items - DONE');
-    };
+  /* * * * * * * * Visualizer life cycle callbacks * * * * * * * */
+  PetrinetVisualizationControl.prototype.destroy = function () {
+    this._detachClientEventListeners();
+    this._removeToolbarItems();
+  };
 
-    PetrinetVisualizationControl.prototype._onLoad = function (gmeId) {
-        var description = this._getObjectDescriptor(gmeId);
-        this._widget.addNode(description);
-    };
+  PetrinetVisualizationControl.prototype._attachClientEventListeners = function () {
+    this._detachClientEventListeners();
+    WebGMEGlobal.State.on(
+      "change:" + CONSTANTS.STATE_ACTIVE_OBJECT,
+      this._stateActiveObjectChanged,
+      this
+    );
+  };
 
-    PetrinetVisualizationControl.prototype._onUpdate = function (gmeId) {
-        var description = this._getObjectDescriptor(gmeId);
-        this._widget.updateNode(description);
-    };
+  PetrinetVisualizationControl.prototype._detachClientEventListeners = function () {
+    WebGMEGlobal.State.off(
+      "change:" + CONSTANTS.STATE_ACTIVE_OBJECT,
+      this._stateActiveObjectChanged
+    );
+  };
 
-    PetrinetVisualizationControl.prototype._onUnload = function (gmeId) {
-        this._widget.removeNode(gmeId);
-    };
+  PetrinetVisualizationControl.prototype.onActivate = function () {
+    this._attachClientEventListeners();
+    this._displayToolbarItems();
 
-    PetrinetVisualizationControl.prototype._stateActiveObjectChanged = function (model, activeObjectId) {
-        if (this._currentNodeId === activeObjectId) {
-            // The same node selected as before - do not trigger
-        } else {
-            this.selectedObjectChanged(activeObjectId);
-        }
-    };
+    if (typeof this._currentNodeId === "string") {
+      WebGMEGlobal.State.registerActiveObject(this._currentNodeId, {
+        suppressVisualizerFromNode: true,
+      });
+    }
+  };
 
-    /* * * * * * * * Visualizer life cycle callbacks * * * * * * * */
-    PetrinetVisualizationControl.prototype.destroy = function () {
-        this._detachClientEventListeners();
-        this._removeToolbarItems();
-    };
+  PetrinetVisualizationControl.prototype.onDeactivate = function () {
+    this._detachClientEventListeners();
+    this._hideToolbarItems();
+  };
 
-    PetrinetVisualizationControl.prototype._attachClientEventListeners = function () {
-        this._detachClientEventListeners();
-        WebGMEGlobal.State.on('change:' + CONSTANTS.STATE_ACTIVE_OBJECT, this._stateActiveObjectChanged, this);
-    };
+  /* * * * * * * * * * Updating the toolbar * * * * * * * * * */
+  PetrinetVisualizationControl.prototype._displayToolbarItems = function () {
+    if (this._toolbarInitialized === true) {
+      for (var i = this._toolbarItems.length; i--; ) {
+        this._toolbarItems[i].show();
+      }
+    } else {
+      this._initializeToolbar();
+    }
+  };
 
-    PetrinetVisualizationControl.prototype._detachClientEventListeners = function () {
-        WebGMEGlobal.State.off('change:' + CONSTANTS.STATE_ACTIVE_OBJECT, this._stateActiveObjectChanged);
-    };
+  PetrinetVisualizationControl.prototype._hideToolbarItems = function () {
+    if (this._toolbarInitialized === true) {
+      for (var i = this._toolbarItems.length; i--; ) {
+        this._toolbarItems[i].hide();
+      }
+    }
+  };
 
-    PetrinetVisualizationControl.prototype.onActivate = function () {
-        this._attachClientEventListeners();
-        this._displayToolbarItems();
+  PetrinetVisualizationControl.prototype._removeToolbarItems = function () {
+    if (this._toolbarInitialized === true) {
+      for (var i = this._toolbarItems.length; i--; ) {
+        this._toolbarItems[i].destroy();
+      }
+    }
+  };
 
-        if (typeof this._currentNodeId === 'string') {
-            WebGMEGlobal.State.registerActiveObject(this._currentNodeId, {suppressVisualizerFromNode: true});
-        }
-    };
+  PetrinetVisualizationControl.prototype._initializeToolbar = function () {
+    var self = this,
+      toolBar = WebGMEGlobal.Toolbar;
 
-    PetrinetVisualizationControl.prototype.onDeactivate = function () {
-        this._detachClientEventListeners();
-        this._hideToolbarItems();
-    };
+    this._toolbarItems = [];
 
-    /* * * * * * * * * * Updating the toolbar * * * * * * * * * */
-    PetrinetVisualizationControl.prototype._displayToolbarItems = function () {
+    this._toolbarItems.push(toolBar.addSeparator());
 
-        if (this._toolbarInitialized === true) {
-            for (var i = this._toolbarItems.length; i--;) {
-                this._toolbarItems[i].show();
-            }
-        } else {
-            this._initializeToolbar();
-        }
-    };
+    /************** Go to hierarchical parent button ****************/
+    this.$btnModelHierarchyUp = toolBar.addButton({
+      title: "Go to parent",
+      icon: "glyphicon glyphicon-circle-arrow-up",
+      clickFn: function (/*data*/) {
+        WebGMEGlobal.State.registerActiveObject(self._currentNodeParentId);
+      },
+    });
+    this._toolbarItems.push(this.$btnModelHierarchyUp);
+    this.$btnModelHierarchyUp.hide();
 
-    PetrinetVisualizationControl.prototype._hideToolbarItems = function () {
+    /************** Checkbox example *******************/
 
-        if (this._toolbarInitialized === true) {
-            for (var i = this._toolbarItems.length; i--;) {
-                this._toolbarItems[i].hide();
-            }
-        }
-    };
+    this.$cbShowConnection = toolBar.addCheckBox({
+      title: "toggle checkbox",
+      icon: "gme icon-gme_diagonal-arrow",
+      checkChangedFn: function (data, checked) {
+        self._logger.debug("Checkbox has been clicked!");
+      },
+    });
+    this._toolbarItems.push(this.$cbShowConnection);
 
-    PetrinetVisualizationControl.prototype._removeToolbarItems = function () {
+    this._toolbarInitialized = true;
+  };
 
-        if (this._toolbarInitialized === true) {
-            for (var i = this._toolbarItems.length; i--;) {
-                this._toolbarItems[i].destroy();
-            }
-        }
-    };
-
-    PetrinetVisualizationControl.prototype._initializeToolbar = function () {
-        var self = this,
-            toolBar = WebGMEGlobal.Toolbar;
-
-        this._toolbarItems = [];
-
-        this._toolbarItems.push(toolBar.addSeparator());
-
-        /************** Go to hierarchical parent button ****************/
-        this.$btnModelHierarchyUp = toolBar.addButton({
-            title: 'Go to parent',
-            icon: 'glyphicon glyphicon-circle-arrow-up',
-            clickFn: function (/*data*/) {
-                WebGMEGlobal.State.registerActiveObject(self._currentNodeParentId);
-            }
-        });
-        this._toolbarItems.push(this.$btnModelHierarchyUp);
-        this.$btnModelHierarchyUp.hide();
-
-        /************** Checkbox example *******************/
-
-        this.$cbShowConnection = toolBar.addCheckBox({
-            title: 'toggle checkbox',
-            icon: 'gme icon-gme_diagonal-arrow',
-            checkChangedFn: function (data, checked) {
-                self._logger.debug('Checkbox has been clicked!');
-            }
-        });
-        this._toolbarItems.push(this.$cbShowConnection);
-
-        this._toolbarInitialized = true;
-    };
-
-    return PetrinetVisualizationControl;
+  return PetrinetVisualizationControl;
 });
