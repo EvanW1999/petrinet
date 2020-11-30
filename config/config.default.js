36216ba172ffdc5c12bdc4880b4832da508b358f
Evan Wang <evan.l.wang@vanderbilt.edu>
2020-11-30 01:13:39 -0500

    Forgot to update seed!

--- config/config.default.js
+++ config/config.default.js
@@ -5,7 +5,7 @@ var config = require("./config.webgme"),
 
 // Add/overwrite any additional settings here
 // config.server.port = 8080;
-config.mongo.uri = "mongodb://127.0.0.1:32768/webgme_my_app";
+// config.mongo.uri = "mongodb://127.0.0.1:32768/webgme_my_app";
 config.plugin.allowServerExecution = true;
 
 config.requirejsPaths["jointjs"] = "./node_modules/jointjs/dist/joint.min";
